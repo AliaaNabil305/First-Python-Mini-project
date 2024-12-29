e2071013b150e8d84096b59c58aee04d19384aad
Aliaa Nabil <114958840+AliaaNabil305@users.noreply.github.com>
2024-12-29 21:55:28 +0200

    Add files via upload

---
+++ Python Tutorial/first.py
@@ -0,0 +1,4 @@
+name="Aliaa Nabil Mahmoud"
+age=24
+summary_of_what_i_want_to_learn_in_this_project="i want to learn how to use pyhton smoothly to be able to make alot of projects using this simple language"
+print(f"my name is {name} ,age {age} and  summary of what you want to learn in this project:{summary_of_what_i_want_to_learn_in_this_project}")
