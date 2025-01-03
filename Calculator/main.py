ef50d24b9595c549beaa8c701b7870ff1aa40b27
Aliaa Nabil <114958840+AliaaNabil305@users.noreply.github.com>
2025-01-04 01:11:33 +0200

    Add files via upload

---
+++ Calculator/main.py
@@ -0,0 +1,7 @@
+first_number=float(input("Enter the first number: "))
+second_number=float(input("Enter the second number: "))
+addition=first_number + second_number
+subtraction=first_number - second_number
+division=first_number / second_number
+multiplication=first_number * second_number
+print(f"the sum is {addition},subtraction is {subtraction},multiplication is {multiplication} and division is {division}")
